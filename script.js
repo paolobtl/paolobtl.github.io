aa4cd9637d8a3de18bdd09dd8a1a81f60e4ceb45
paolobtl <bietolinipaolo@gmail.com>
2023-04-14 15:55:08 +0200

    minor style fixes + email

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('#menu').click(function(){
+    $('#menu').click(function () {
         $(this).toggleClass('fa-times');
         $('header').toggleClass('toggle')
     })
 
-    $(window).on('scroll load', function(){
+    $(window).on('scroll load', function () {
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
 
-        if($(window).scrollTop() > 20){
+        if ($(window).scrollTop() > 20) {
             $('.top').show();
         } else {
             $('.top').hide();
         }
     })
 
-    $('a[href*="#"]').on('click',function(e){
+    $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({
-            scrollTop : $($(this).attr('href')).offset().top,
-        },
+                scrollTop: $($(this).attr('href')).offset().top,
+            },
             500,
             'linear'
         )
     })
+
+    $('.hiddenMail').on('click', function (event) {
+        event.preventDefault();
+        $(this).off("click");
+        var email = $(this).attr("data-email").replace(/AT/, '@').replace(/DOT/, '.');
+        $(this).removeClass("hiddenMail");
+        $(this).html(email);
+        $(this).attr("href", "mailto:" + email);
+    });
 })
+
+
