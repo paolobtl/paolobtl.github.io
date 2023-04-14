961bedf9ce71d5da8c3e66cb65bb0174dffc4953
paolobtl <bietolinipaolo@gmail.com>
2023-04-14 13:20:14 +0200

    redesign

--- script.js
+++ script.js
@@ -1,47 +1,28 @@
-const subnav = document.querySelector('.subnav');
-const dropdown = document.querySelector('.dropdown');
-const social = document.querySelector('#social');
-const footerDate = document.querySelector("body > footer > span:nth-child(2)");
-const themeSwitcher = document.querySelector('.theme-switcher');
-const themeSwitcherIcon = document.querySelector('.theme-switcher-icon');
-const date = new Date();
-footerDate.textContent = date.getFullYear();
-const toggleDropdown = () => {
-    if (!dropdown.classList.contains('dropdown-open')) {
-        subnav.style.height = 'fit-content';
-        subnav.style.opacity = '1';
-        dropdown.classList.add('dropdown-open');
-        social.textContent = 'Social-';
-    } else {
-        subnav.style.height = '0';
-        subnav.style.opacity = '0';
-        dropdown.classList.remove('dropdown-open');
-        social.textContent = 'Social+';
-    }
-};
+$(document).ready(function(){
 
-const toggleTheme = () => {
-    if (!themeSwitcher.classList.contains('dark-mode')) {
-        document.body.style.backgroundColor = 'var(--dark-bg)';
-        document.body.style.color = 'var(--dark-text)'
-        themeSwitcher.classList.add('dark-mode');
-        themeSwitcherIcon.style.filter = 'invert(1)';
-    } else {
-        document.body.style.backgroundColor = 'var(--light-bg)';
-        document.body.style.color = 'var(--light-text)'
-        themeSwitcher.classList.remove('dark-mode');
-        themeSwitcherIcon.style.filter = 'invert(0)';
-    }
-}
-dropdown.addEventListener('click', toggleDropdown);
-document.querySelectorAll('a:not([href*="#"])').forEach(anchor => {
-    if (!anchor.href == document.location.href) {
-        anchor.setAttribute('target', '_blank');
-        anchor.setAttribute('rel', 'noopener noreferrer');
-    }
-});
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle')
+    })
 
-themeSwitcherIcon.addEventListener('click', toggleTheme)
+    $(window).on('scroll load', function(){
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
 
-// Set subnav closed and social text to 'Social+'
-social.textContent = 'Social+';
+        if($(window).scrollTop() > 20){
+            $('.top').show();
+        } else {
+            $('.top').hide();
+        }
+    })
+
+    $('a[href*="#"]').on('click',function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop : $($(this).attr('href')).offset().top,
+        },
+            500,
+            'linear'
+        )
+    })
+})
