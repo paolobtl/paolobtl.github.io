93720b2b014aa79321819cb44c94989f1cb25b1e
paolobtl <bietolinipaolo@gmail.com>
2023-05-13 23:13:51 +0200

    theme selector - set light mode default

--- js/jquery.js
+++ js/jquery.js
@@ -31,49 +31,4 @@ $(document).ready(function () {
       $(this).attr("href", "mailto:" + email);
     });
   
-    /* Dark Mode */
-    var isLightMode = false; // check if dark mode is enabled by default
-  
-    // check if user has previously selected dark mode
-    if (localStorage.getItem('isLightMode') === 'true') {
-      $('body').addClass('dark-mode'); // add 'dark-mode' class to body
-      $('.fa-sun').removeClass('fa-sun').addClass('fa-moon'); // change icon class
-      isLightMode = true; // set isLightMode to true
-    }
-  
-    // toggle dark mode when theme selector is clicked
-    $('.theme-selector').click(function () {
-      isLightMode = !isLightMode; // toggle isLightMode value
-  
-      // update icon class and body class based on isLightMode value
-      if (isLightMode) {
-        $('.fa-sun').removeClass('fa-sun').addClass('fa-moon');
-        $('body').addClass('dark-mode');
-      } else {
-        $('.fa-moon').removeClass('fa-moon').addClass('fa-sun');
-        $('body').removeClass('dark-mode');
-      }
-  
-      // save isLightMode preference in LocalStorage
-      localStorage.setItem('isLightMode', isLightMode);
-      let lSisLightMode = localStorage.getItem('isLightMode');
-      // update CSS variables to invert colors
-      if (isLightMode) {
-        $(':root').css('--yellow', '#f9ca24');
-        $(':root').css('--white', '#333');
-        $(':root').css('--white2', '#222');
-        $(':root').css('--dark', '#e1e1e1');
-        $(':root').css('--dark2', '#fff');
-        $(':root').css('--black', '#fff');
-        $(':root').css('--header-bg', '#eee');
-      } else {
-        $(':root').css('--yellow', '#f9ca24');
-        $(':root').css('--white', '#fff');
-        $(':root').css('--white2', '#eee');
-        $(':root').css('--dark', '#333');
-        $(':root').css('--dark2', '#222');
-        $(':root').css('--black', '#212427');
-        $(':root').css('--header-bg', '#1a1a1a');
-      }
-    });
   });
--- js/vanillajs.js
+++ js/vanillajs.js
@@ -1,9 +1,38 @@
 // Footer year 
 document.getElementById('currentYear').innerHTML = new Date().getFullYear();
 
+// Set up variables for the theme modes
+const lightTheme = {
+    '--white': '#fff',
+    '--white2': '#eee',
+    '--dark': '#333',
+    '--dark2': '#222'
+};
 
+const darkTheme = {
+    '--white': '#333',
+    '--white2': '#222',
+    '--dark': '#fff',
+    '--dark2': '#eee'
+};
 
-window.addEventListener("DOMContentLoaded", (event) => {
+// Define a function to set the theme
+const setTheme = (themeMode) => {
+    const theme = themeMode === 'light' ? lightTheme : darkTheme;
+    Object.entries(theme).forEach(([property, value]) => {
+        document.documentElement.style.setProperty(property, value);
+    });
+    window.localStorage.setItem('themeMode', themeMode);
+};
+
+// Toggle theme mode on button click
+const toggleThemeMode = () => {
+    const themeMode = theme() === 'dark' ? 'light' : 'dark';
+    setTheme(themeMode);
+};
+
+// Check theme mode on page load
+const onLoad = () => {
     console.log("DOM fully loaded and parsed");
     // Hide blog section
     if (document.querySelector("#blog")) {
@@ -11,5 +40,19 @@ window.addEventListener("DOMContentLoaded", (event) => {
             document.querySelector("#blog").remove();
             document.querySelector("#top > header > nav > ul > li:nth-child(2) > a").remove()
         };
+    };
+    const currentTheme = theme();
+    if (currentTheme) {
+        setTheme(currentTheme);
+    } else {
+        setTheme('light');
     }
-});
+};
+
+// Define the theme function to get the current theme mode
+const theme = () => window.localStorage.getItem('themeMode');
+
+// Attach event listeners
+const themeModeSelektor = document.querySelector('#top i.fa-sun');
+themeModeSelektor.addEventListener('click', toggleThemeMode);
+window.addEventListener('DOMContentLoaded', onLoad);
